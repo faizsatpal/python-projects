87b78cf6832c28a2531d2d82498364f413bb3dea
FAIZ SATPAL ATOKO <116289022+faizsatpal@users.noreply.github.com>
2024-11-05 23:36:00 -0800

    Add files via upload

---
+++ python project/input.py
@@ -0,0 +1,9 @@
+# user_input.py
+
+# Collecting user input
+name = input("What's your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+
+# Displaying a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
